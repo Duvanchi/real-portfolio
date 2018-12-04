cfe5e098fe5e7ea5321ab7360a1ba06e5ed983f2
Duvanchi <cochize0@gmail.com>
2018-12-03 20:13:28 -0500

    Way too much for an initial commit

---
+++ routes/post-api-routes.js
@@ -0,0 +1,11 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+};
